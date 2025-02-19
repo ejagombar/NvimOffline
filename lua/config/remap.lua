f726b9e6e8af38eac5532d78ba0de9406e1bd29c
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2025-02-19 21:31:06 +0000

    Create base config and add one plugin

---
+++ lua/config/remap.lua
@@ -0,0 +1,33 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set({ "n", "v" }, "<Leader>e", "%")
+
+vim.keymap.set("n", "cp", "+y")
+vim.keymap.set("n", "cv", "+p")
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Dont put deleted items into buffer when using x
+vim.keymap.set("n", "x", '"_x')
+vim.keymap.set("v", "x", '"_x')
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<leader>rp", ":! ./runcommand")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
---
+++ lua/config/set.lua
@@ -0,0 +1,59 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = false
+
+vim.opt.scrolloff = 10
+
+vim.opt.updatetime = 50
+
+vim.opt.timeoutlen = 300
+
+vim.opt.signcolumn = "yes"
+
+vim.o.cursorline = true
+
+vim.opt.swapfile = false
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.g.loaded_perl_provider = false -- disable warning in :checkhealth
+
+vim.opt.inccommand = "split"
+
+vim.opt.textwidth = 0
+
+vim.filetype.add({
+	extension = {
+		vert = "vert",
+		frag = "frag",
+		geom = "geom",
+	},
+})
+
+vim.cmd("autocmd BufEnter * set formatoptions-=cro")
+vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
+
+vim.cmd([[
+augroup CursorLineNrHighlight
+  autocmd!
+  autocmd VimEnter * highlight CursorLineNr guibg=bg guifg=#CCCCCC
+  autocmd VimEnter * highlight CursorLine ctermbg=NONE ctermfg=NONE guibg=NONE guifg=NONE
+augroup END
+]])
