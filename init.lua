1cb617ffaaf1c8a49f5371d676ba72cf1fe70a2d
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2025-06-01 14:22:27 +0100

    Updates

--- init.lua
+++ init.lua
@@ -1,4 +1,100 @@
-require("config")
+-- =================================== Neovim Keybinds ===================================
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Centers the cursor when going ctrl + u or d
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Dont put deleted items into buffer when using x
+vim.keymap.set("n", "x", '"_x')
+vim.keymap.set("v", "x", '"_x')
+
+-- Remove search highlighting by pressing enter
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+
+-- Replace the currently highlighted text with the string in your default copy buffer
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- =================================== Neovim Settings ===================================
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = false
+
+vim.opt.scrolloff = 10
+
+vim.opt.updatetime = 50
+
+vim.opt.timeoutlen = 300
+
+vim.opt.signcolumn = "yes"
+
+vim.o.cursorline = true
+
+vim.opt.swapfile = false
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.g.loaded_perl_provider = false -- disable warning in :checkhealth
+
+vim.opt.inccommand = "split"
+
+vim.opt.textwidth = 0
+
+vim.cmd("autocmd BufEnter * set formatoptions-=cro")
+vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
+
+vim.cmd([[
+augroup CursorLineNrHighlight
+  autocmd!
+  autocmd VimEnter * highlight CursorLineNr guibg=bg guifg=#CCCCCC
+  autocmd VimEnter * highlight CursorLine ctermbg=NONE ctermfg=NONE guibg=NONE guifg=NONE
+augroup END
+]])
+
+vim.filetype.add({
+	extension = {
+		module = "module",
+		library = "library",
+		script = "script",
+	},
+})
+
+-- Run pfile on the current file. Mnemonic: Helix Edit
+vim.keymap.set("n", "<leader>he", function()
+	local filepath = vim.fn.expand("%")
+	if filepath == "" then
+		vim.notify("No file in buffer to pfile!", vim.log.levels.WARN)
+		return
+	end
+
+	local cmd = { "bash", "-c", ". /tsl/devtools/profile/paliases && pfile " .. vim.fn.shellescape(filepath) }
+	vim.notify(vim.fn.system(cmd))
+end)
+
+-- =================================== Plugin Settings ===================================
+
+vim.opt.packpath:append("/tsl/devtools/nvim/nvim-plugins") -- Do not delete!
 
 ------------------ telescope ------------------
 
@@ -7,7 +103,16 @@ local builtin = require("telescope.builtin")
 
 telescope.setup({
 	defaults = {
-		file_ignore_patterns = { "node_modules", "build", ".git/", "venv", ".venv", "__pycache__" },
+		file_ignore_patterns = {
+			"node_modules",
+			"build",
+			".git/",
+			"venv",
+			".venv",
+			"__pycache__",
+			"compile_commands.json",
+			".eml",
+		},
 	},
 	pickers = {
 		find_files = { hidden = true },
@@ -60,8 +165,11 @@ vim.cmd("colorscheme onedark")
 local nvimtree = require("nvim-tree")
 local api = require("nvim-tree.api")
 
+-- Comment these out to re-enable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+vim.g.nvim_tree_respect_buf_cwd = 1
 
 local function my_on_attach(bufnr)
 	-- default mappings
@@ -72,10 +180,53 @@ local function my_on_attach(bufnr)
 	vim.keymap.set("n", "?", api.tree.toggle_help)
 end
 
+local HEIGHT_RATIO = 0.8
+local WIDTH_RATIO = 0.75
+
 nvimtree.setup({
+	disable_netrw = true,
+	hijack_netrw = true,
+	sync_root_with_cwd = true,
+	update_cwd = true,
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+	},
+	view = {
+		relativenumber = true,
+		float = {
+			enable = true,
+			open_win_config = function()
+				local screen_w = vim.opt.columns:get()
+				local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+				local window_w = screen_w * WIDTH_RATIO
+				local window_h = screen_h * HEIGHT_RATIO
+				local window_w_int = math.floor(window_w)
+				local window_h_int = math.floor(window_h)
+				local center_x = (screen_w - window_w) / 2
+				local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+				return {
+					border = "rounded",
+					relative = "editor",
+					row = center_y,
+					col = center_x,
+					width = window_w_int,
+					height = window_h_int,
+				}
+			end,
+		},
+		width = function()
+			return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+		end,
+	},
+
 	on_attach = my_on_attach,
 	git = {
 		enable = false,
+	},
+	renderer = { icons = { show = { file = false, folder = false, folder_arrow = false } } },
+	filters = {
+		custom = { "*.o", "*.lo" },
 	},
 })
 
@@ -89,11 +240,14 @@ cmp.setup({
 	completion = { completeopt = "menu,menuone,noinsert" },
 
 	mapping = cmp.mapping.preset.insert({
-		["<C-n>"] = cmp.mapping.select_next_item(),
-		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<Tab>"] = cmp.mapping.select_next_item(),
+		["<S-Tab>"] = cmp.mapping.select_prev_item(),
+		-- ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+		-- ["<C-n>"] = cmp.mapping.select_next_item(),
+		-- ["<C-p>"] = cmp.mapping.select_prev_item(),
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-y>"] = cmp.mapping.confirm({ select = true }),
 		["<C-Space>"] = cmp.mapping.complete({}),
 	}),
 	sources = {
@@ -106,7 +260,9 @@ cmp.setup({
 
 local lspconfig = require("lspconfig")
 
-local diagnostic_signs = { Error = "", Warn = "", Hint = "󰠠", Info = "" }
+-- local diagnostic_signs = { Error = "", Warn = "", Hint = "󰠠", Info = "" } -- Use this if you do not have a font that symbols
+local diagnostic_signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
+
 for type, icon in pairs(diagnostic_signs) do
 	vim.fn.sign_define("DiagnosticSign" .. type, { text = icon, texthl = "DiagnosticSign" .. type })
 end
@@ -148,6 +304,25 @@ lspconfig.clangd.setup({
 	},
 	filetypes = { "cpp", "c", "hpp", "cc", "cxx", "hxx", "h" },
 	capabilities = capabilities,
+	on_attach = function(client, bufnr)
+		-- client.server_capabilities.semanticTokensProvider = nil -- Disable lsp highlighting outright. Will do this once we have the cpp treesitter parser installed. The C one does not work great.
+	end,
+})
+
+------------------ nvim-treesitter ------------------
+
+local filetypes = { "c", "cpp", "library", "module", "script", "hpp" }
+vim.treesitter.language.register("c", filetypes)
+
+local ts_aug = vim.api.nvim_create_augroup("AutoTSStart", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = ts_aug,
+	pattern = filetypes,
+	callback = function(args)
+		local parser = (args.match == "cpp") and "cpp" or "c"
+		vim.treesitter.start(args.buf, "c")
+	end,
 })
 
 ------------------ undotree ------------------
@@ -183,12 +358,6 @@ end)
 vim.keymap.set("n", "<leader>l", function()
 	harpoon:list():select(4)
 end)
-vim.keymap.set("n", "<C-n>", function()
-	harpoon:list():prev()
-end)
-vim.keymap.set("n", "<C-p>", function()
-	harpoon:list():next()
-end)
 
 ------------------ tmux ------------------
 
@@ -220,8 +389,5 @@ tmux.setup({
 })
 
 ------------------ Other Setup ------------------
-
--- Setup extension helptags
-vim.cmd("helptags ALL")
 
 require("telescope").load_extension("fzf")
